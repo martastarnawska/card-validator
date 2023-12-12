c3d4536e1c4e7e17b3409d605dbad30c54a6a817
martastarnawska <marta.chelm@gmail.com>
2023-12-12 10:01:33 -0500

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
 import checkIsValid from './card-validator';
 
 test('validates card number correctly', () => {
-  const isCardNumberValid = checkIsValid("5555555555554444")
+  const [isCardNumberValid] = checkIsValid("5555555555554444")
 
   expect(isCardNumberValid).toBeTruthy()
 })
 
-test('validates proper card number correctly', () => {
-  const isCardNumberValid = checkIsValid("5555555555554447")
+test('validates invalid card number correctly', () => {
+  const [isCardNumberValid] = checkIsValid("5555555555554447")
 
   expect(isCardNumberValid).toBeFalsy()
 })
