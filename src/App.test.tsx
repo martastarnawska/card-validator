e558e1398a85c7a4ce41b88bff3598cb0d363947
martastarnawska <marta.chelm@gmail.com>
2024-01-04 10:42:38 -0500

    update tests to check also validation message

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,23 @@
 import checkIsValid from './card-validator';
+import { MESSAGES } from './card-validator'
 
 test('validates card number correctly', () => {
-  const [isCardNumberValid] = checkIsValid("5555555555554444")
+  const [isCardNumberValid, validationMessage] = checkIsValid("5555555555554444")
 
   expect(isCardNumberValid).toBeTruthy()
+  expect(validationMessage).toEqual(MESSAGES.OK)
 })
 
 test('validates invalid card number correctly', () => {
-  const [isCardNumberValid] = checkIsValid("5555555555554447")
+  const [isCardNumberValid, validationMessage] = checkIsValid("5555555555554447")
 
   expect(isCardNumberValid).toBeFalsy()
+  expect(validationMessage).toEqual(MESSAGES.INVALID)
+})
+
+test('validates provided text correctly', () => {
+  const [isCardNumberValid, validationMessage] = checkIsValid("abc")
+
+  expect(isCardNumberValid).toBeFalsy()
+  expect(validationMessage).toEqual(MESSAGES.NOT_NUMBER)
 })
